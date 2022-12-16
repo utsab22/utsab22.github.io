575667db639cd83842146a09fa24b9da9497058a
Utsab Sapkota <utsab.sapkota46@gmail.com>
2022-12-16 10:08:49 -0600

    j

--- w3d5/q4/index.js
+++ w3d5/q4/index.js
@@ -3,7 +3,6 @@ const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/css", express.static(path.join(__dirname, "css")));
